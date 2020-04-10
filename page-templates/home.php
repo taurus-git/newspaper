f46290d3e343cb69403150ec38af83d10d3c88ed
taurus-git <t.1113@ya.ru>
2020-04-10 17:26:18 +0300

    Added function for viewing popular posts

--- page-templates/home.php
+++ page-templates/home.php
@@ -9,7 +9,10 @@ get_header();
 <?php while (have_posts()): the_post();?>
 
     <?php the_content();?>
-
+    <ul>
+        <?php $most_viewed_posts = kama_get_most_viewed("num=10 &echo=0 &return=array"); ?>
+        <?php var_dump( $most_viewed_posts );?>
+    </ul>
 <?php endwhile; ?>
 
 <?php get_sidebar(); ?>
