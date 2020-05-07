d396dc0950681ea1e1762af50ab88a959f78484a
taurus-git <t.1113@ya.ru>
2020-05-07 15:38:38 +0300

    Acf code added.

--- inc/theme_options.php
+++ inc/theme_options.php
@@ -24,7 +24,6 @@ if (function_exists('acf_add_options_page') ) {
 }
 
 if( function_exists('acf_add_local_field_group') ):
-
     acf_add_local_field_group(array(
         'key' => 'group_5eb3c852b6a4a',
         'title' => 'Footer',
@@ -132,4 +131,51 @@ if( function_exists('acf_add_local_field_group') ):
         'description' => '',
     ));
 
+    acf_add_local_field_group(array(
+        'key' => 'group_5eb3d23aa33de',
+        'title' => 'Header logo',
+        'fields' => array(
+            array(
+                'key' => 'field_5eb3d242df41b',
+                'label' => 'Header logo',
+                'name' => 'header_logo',
+                'type' => 'image',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'return_format' => 'array',
+                'preview_size' => 'medium',
+                'library' => 'all',
+                'min_width' => '',
+                'min_height' => '',
+                'min_size' => '',
+                'max_width' => '',
+                'max_height' => '',
+                'max_size' => '',
+                'mime_types' => '',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'options_page',
+                    'operator' => '==',
+                    'value' => 'acf-options-header',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
 endif;
